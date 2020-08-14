c0d653332293e0fb20147d574d57d0ada0c98a1f
tracy <843358687@qq.com>
2020-08-14 16:42:54 +0800

    完成项目搭建

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from './logo.svg'
+import { Link } from 'react-router-dom'
+function Header() {
+    return (
+        <header>
+            <img src={Logo} />
+            <nav>
+                <Link to='/'>首页</Link>
+                <Link to='/history'>历史</Link>
+                <Link to='/about'>关于我</Link>
+            </nav>
+        </header>
+    )
+}
+export default Header
