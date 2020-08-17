94589ee66add61ca12893f9c5c16129dcb83a032
tracy <843358687@qq.com>
2020-08-17 16:22:05 +0800

    添加 注册、注销、登录 按钮

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 //import LogoUrl from './logo.svg'
-import { NavLink } from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import styled from 'styled-components'
+import {Button} from "antd";
+
 function ComponentHeader() {
-    return (
-        <Header>
-            <nav>
-                <StyledLink to='/' activeClassName='active' exact >首页</StyledLink>
-                <StyledLink to='/hiStory' activeClassName='active'>历史</StyledLink>
-                <StyledLink to='/about' activeClassName='active'>关于我</StyledLink>
-            </nav>
-            <Login>
-                <Button>
-                    <StyledLink to='/login'>登录</StyledLink>
-                </Button>
-                <Button>
-                    <StyledLink to='/register'>注册</StyledLink>
-                </Button>
-            </Login>
-        </Header>
-    )
+  const [isLogin, setIsLogin] = useState(false)
+  return (
+    <Header>
+      <nav>
+        <StyledLink to='/' activeClassName='active' exact>首页</StyledLink>
+        <StyledLink to='/hiStory' activeClassName='active'>历史</StyledLink>
+        <StyledLink to='/about' activeClassName='active'>关于我</StyledLink>
+      </nav>
+      <Login>
+        {
+          isLogin ?
+            <>
+              Jack <StyledButton onClick={()=>setIsLogin(false)} >注销</StyledButton>
+            </> :
+            <>
+              <StyledButton onClick={()=>setIsLogin(true)}>登录</StyledButton>
+              <StyledButton>注册</StyledButton>
+            </>
+        }
+      </Login>
+    </Header>
+  )
 }
+
 export default ComponentHeader
+const StyledButton = styled(Button)`
+  margin-left:10px;
+`
 const Login = styled.div`
     margin-left:auto;
-`
-const Button = styled.button`
-    margin-left:10px;
 `
 const Header = styled.header`
     padding:10px 50px;
     display:flex;
-    align-item:center;
+    align-items: center;
     background-color:#f6d186;
     color:#fff;
 `;
 const StyledLink = styled(NavLink)`
     margin-right:30px;
+    color:#333;
     &.active{
-        color:#fff;
+       color:#fff;
     }
 `
