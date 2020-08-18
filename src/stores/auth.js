97f4b4c323211e999c98180c4ca2937e7123db8e
tracy <843358687@qq.com>
2020-08-18 20:58:36 +0800

    auth.js 实现login、register、logout

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,10 +1,10 @@
-import { observable, action } from 'mobx'
-import Modal from '../models'
+import {observable, action} from 'mobx'
+import {Auth} from '../models'
+import UserStore from './user'
 class AuthStore {
-  @observable isLogin = false;
-  @observable isLoading = false;
+
   @observable values = {
-    username: 'jack',
+    username: '',
     password: ''
   }
 
@@ -21,27 +21,37 @@ class AuthStore {
   }
 
   @action login() {
-    console.log('登录中...')
-    this.isLoading = true
-    setTimeout(() => {
-      console.log('登录成功')
-      this.isLogin = true
-      this.isLoading = false
-    }, 1000)
+    return new Promise((resolve, reject) => {
+      Auth.login(this.values.username, this.values.password)
+        .then(user => {
+          UserStore.pullUser();
+          resolve(user)
+        })
+        .catch(err => {
+          UserStore.resetUser()
+          reject(err)
+        })
+    })
   }
 
   @action register() {
-    console.log('注册中...')
-    this.isLoading = true
-    setTimeout(() => {
-      console.log('注册成功')
-      this.isLogin = true
-      this.isLoading = false
-    }, 1000)
+    return new Promise((resolve, reject) => {
+      Auth.register(this.values.username, this.values.password)
+        .then(user => {
+          UserStore.pullUser();
+          resolve(user)
+        })
+        .catch(err => {
+          UserStore.resetUser()
+          reject(err)
+        })
+    })
   }
 
   @action logout() {
-    console.log('已注销')
+    Auth.logout()
+    UserStore.resetUser()
   }
 }
-export { AuthStore }
+
+export default new AuthStore()
