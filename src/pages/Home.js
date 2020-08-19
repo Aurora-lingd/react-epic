db4c43a85706d85ed8d6290b9de8f439239aeb8b
tracy <843358687@qq.com>
2020-08-19 16:08:03 +0800

    上传文件初步完成

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import {observer} from 'mobx-react'
 import {useStores} from "../stores";
+import Uploader from  '../components/Uploader'
 
 const Home = observer(() => {
   const {UserStore} = useStores()
   const User = ()=><>Hello {UserStore.currentUser.attributes.username}</>
   return (
-      <h1>
+      <>
         {
           UserStore.currentUser ? <User/>:'用户没有登录'
         }
-      </h1>
+        <Uploader/>
+      </>
   )
 })
 export default Home
